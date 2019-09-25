3ba8f94126d01f71863e07d436789fd81db2350c
kubing <edkt973@gmail.com>
2019-09-25 18:08:18 +0800

    update iconv bat

--- Encoding/ConvertDataEncodingByIconv - UTF8 to UTF16LE.bat
+++ Encoding/ConvertDataEncodingByIconv - UTF8 to UTF16LE.bat
@@ -2,7 +2,7 @@
 
 Set Source=D:\Documents\PendingSources\
 Set Destination=D:\Documents\ConvertedData\
-Set Suffix=_utf8.csv
+Set Suffix=_utf16.csv
 
 :: Clear the destination folder
 Cd %Destination%
@@ -10,6 +10,6 @@ Del /F /Q *.csv
 
 Cd %Source%
 For /R %%i in (*.csv) do (
-    iconv -f utf-16 -t utf-8 %%i > %Destination%%%~ni%Suffix%
+    iconv -f utf-8 -t utf-16le %%i > %Destination%%%~ni%Suffix%
     rem echo %Destination%%%~ni%Suffix%
 )
